65ec7c1be625afa0d583504c8f64434a5f8ec5f4
Jon Yonke <jon.yonke@gmail.com>
2022-11-05 11:21:36 -0400

    build update

--- build.ps1
+++ build.ps1
@@ -1,7 +1,11 @@
 $IPXEGIT = "https://github.com/ipxe/ipxe"
-$IPXEGIT_Folder = Join-Path $Home 'tmp/ipxe-legacy'
 $BuildFolder = Join-Path $PSScriptRoot 'builds'
 
+#--------------------------------------------
+#   BIOS and EFI
+#--------------------------------------------
+$IPXEGIT_Folder = Join-Path $Home 'tmp/ipxe_build'
+Get-ChildItem -Path $IPXEGIT_Folder -Recurse | Remove-Item -Force -Recurse
 
 if (-Not(Test-Path $BuildFolder)) {
     New-Item -ItemType Directory -Path $BuildFolder -Force
@@ -20,52 +24,27 @@ else {
     Set-Location (Join-Path $IPXEGIT_Folder 'src')
 }
 
-# Get current header and script from fogproject repo
+#SSL Bug Workaround
+#https://github.com/ipxe/ipxe/issues/606#issuecomment-1057367152
+Get-ChildItem -Path (Join-Path $PSScriptRoot 'certificates') -File | Copy-Item -Destination ./
+$CERTS = (Get-ChildItem ./ -Filter *.pem).FullName -join ','
+
+# Update Configs
 Write-Host "Copy (overwrite) iPXE headers and scripts..."
 Copy-Item -Path "$PSScriptRoot/scripts/default.ipxe" -Destination .
-Copy-Item -Path "$PSScriptRoot/config/legacy/general.h" -Destination ./config/
-Copy-Item -Path "$PSScriptRoot/config/legacy/settings.h" -Destination ./config/
-Copy-Item -Path "$PSScriptRoot/config/legacy/console.h" -Destination ./config/
+Copy-Item -Path "$PSScriptRoot/config/general.h" -Destination ./config/
+Copy-Item -Path "$PSScriptRoot/config//console.h" -Destination ./config/
 
 # Build the files
 try {
-    make bin/undionly.kpxe EMBED=default.ipxe
-    make bin/ipxe.iso EMBED=default.ipxe
-    Get-ChildItem -Path $IPXEGIT_Folder -Recurse -Include *.iso, *.efi, *.kpxe | Move-Item -Destination $BuildFolder -Force
+    make bin/undionly.kpxe bin/ipxe.lkrn bin-x86_64-efi/ipxe.efi EMBED=default.ipxe TRUST=$CERTS CERT=$CERTS
+    ./util/genfsimg -o ipxe.iso bin/ipxe.lkrn bin-x86_64-efi/ipxe.efi
+    #gci ./bin/  -Include ipxe.*, undionly.kpxe -Recurse -Exclude *.tmp, *.map
+    Get-ChildItem -Path $IPXEGIT_Folder -Recurse -Include undionly.kpxe, ipxe.efi, ipxe.iso | Copy-Item -Destination $BuildFolder -Force
 }
 catch {
     $_.Exception.Message
     exit 1
 }
 
-#Build EFI
-$IPXEGIT_Folder = Join-Path $Home 'tmp/ipxe-uefi'
-
-if (Test-Path $IPXEGIT_Folder) {
-    Set-Location -Path $IPXEGIT_Folder
-    git clean -fd
-    git reset --hard
-    git pull
-    Set-Location (Join-Path $IPXEGIT_Folder 'src')
-}
-else {
-    git clone $IPXEGIT $IPXEGIT_Folder
-    Set-Location (Join-Path $IPXEGIT_Folder 'src')
-}
-
-# Get current header and script from fogproject repo
-Write-Host "Copy (overwrite) iPXE headers and scripts..."
-Copy-Item -Path "$PSScriptRoot/scripts/default.ipxe" -Destination .
-Copy-Item -Path "$PSScriptRoot/config/uefi/general.h" -Destination ./config/
-Copy-Item -Path "$PSScriptRoot/config/uefi/settings.h" -Destination ./config/
-Copy-Item -Path "$PSScriptRoot/config/uefi/console.h" -Destination ./config/
-
-# Build the files
-try {
-    make bin-x86_64-efi/ipxe.efi EMBED=default.ipxe
-    Get-ChildItem -Path $IPXEGIT_Folder -Recurse -Include *.iso, *.efi, *.kpxe | Move-Item -Destination $BuildFolder -Force
-}
-catch {
-    $_.Exception.Message
-    exit 1
-}
+Set-Location $PSScriptRoot
